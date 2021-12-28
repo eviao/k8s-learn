01d25141d780c3d62ba85d7d82f8b06101ad1621
eviao <eviao@live.cn>
2021-12-28 13:43:55 +0800

    add HealthController

--- src/usercenter/src/main/java/cn/eviao/k8slearnusercenter/K8sLearnUsercenterApplication.java
+++ src/usercenter/src/main/java/cn/eviao/k8slearnusercenter/K8sLearnUsercenterApplication.java
@@ -6,18 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @ControllerAdvice
 @SpringBootApplication
 public class K8sLearnUsercenterApplication {
-
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello, world!";
-	}
 
 	@ExceptionHandler(ParamsBindingException.class)
 	public ResponseEntity handleParamBindException(ParamsBindingException e) {
