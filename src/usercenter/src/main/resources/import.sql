46d8f6bfef24c45511a60f8c31aa05896d81324b
eviao <eviao@live.cn>
2021-12-29 11:00:55 +0800

    add import.sql

---
+++ src/usercenter/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- users
+insert into user(name, created_at, updated_at) values('user 1', now(), now());
+insert into user(name, created_at, updated_at) values('user 2', now(), now());
+insert into user(name, created_at, updated_at) values('user 3', now(), now());
+insert into user(name, created_at, updated_at) values('user 4', now(), now());
+insert into user(name, created_at, updated_at) values('user 5', now(), now());
